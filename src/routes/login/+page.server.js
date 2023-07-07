9aa9f7c390d316c6a7945912f23e1013b7f7e2be
consultingninja <consultingninja.tech@gmail.com>
2023-07-07 11:20:40 -0500

    update login

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import crypto from 'crypto';
-import {SECRET_SIGNATURE} from '$env/static/private';
+
 
 
 export const actions = {
@@ -9,14 +8,6 @@ export const actions = {
         const loginFormData = await request.formData();
         const email = loginFormData.get('email')?.toString()??'';
         const password = loginFormData.get('password')?.toString()??'';
-        //create a base hash
-        const hash = crypto.createHash('sha256');
-        //add the password
-        hash.update(password);
-        //add the env secret
-        hash.update(SECRET_SIGNATURE);
-        // Obtain the hashed password
-        const hashedPassword = hash.digest('hex');
 
         let loginResponse={
             email,
@@ -27,15 +18,15 @@ export const actions = {
         try{
             await locals.userPb.collection('users').authWithPassword(
                 email,
-                hashedPassword
+                password
             );
             //console.log('auth store',locals.pb.authStore);
  
             
-            if(locals.pb.authStore.baseToken) throw redirect(303,'/auth')
+            if(locals.userPb.authStore.baseToken) throw redirect(303,'/auth')
         }
         finally{
-            if(!locals.pb.authStore.baseToken) return loginResponse
+            if(!locals.userPb.authStore.baseToken) return loginResponse
         }
 
 
